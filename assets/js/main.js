b20c4b5adc50c90976dd82d25d76ba7f7df77387
VivekanandPadala <vivekanand.padala90@gmail.com>
2018-08-20 00:03:19 +0530

    Tech News api for news is done and dusted, added about and contact pages, logos and favicon.

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// Top News
+$(function () {
+    $.ajax({
+          url: "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=b023e7d89f064287b363a6691cd99344",
+          "Content-Type": "application/json",
+          success: function (results) {
+                $.each(results.articles, function(i) {
+                    //if(i < 20) {  
+                var source = results.articles[i].source.name;
+                var title = results.articles[i].title;
+                var description = results.articles[i].description;
+                var image = results.articles[i].urlToImage;
+                var url = results.articles[i].url;
+                var dateString = results.articles[i].publishedAt;
+                var d = new Date(dateString);
+                var date = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+                if (image == null) {
+                    image = "assets/images/techride.png"
+                }
+                $('#topNews').append(
+                      "<div class='col-md-4 cardAlignment'><div class='mainCard'><div class='cardContainer'><h6 class='techHead'>Source: <b><i>" + source + "</i></b> published on " +
+                      date + " </h6></div><hr><img class='newsImage' src='" + image +
+                      "'><div class='cardContainer'><h4 class='mainHeading'>" + title +
+                      "</h4><p class='newsDesc'>" + description + " <a href='" +
+                      url +
+                      "' target='_blank'>Read More..</a></p></div></div></div>"
+                );
+                      //}
+          });
+          }
+    });
+});
+// Top News
